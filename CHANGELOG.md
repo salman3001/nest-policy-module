6a21144fd700548ba4146daed9246bd5054038e6
salman <therodfighter@gmail.com>
2024-06-08 00:36:02 +0400

    added NestPolicyError class

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+## v1.0.2 - 2024-06-08
+
+### Patch
+
+- added NestPolicyError class
+
 ## v1.0.1 - 2024-06-07
 
 ### Bug fixes
---
+++ src/exceptions/NestPolicyError.ts
@@ -0,0 +1,7 @@
+import { HttpException, HttpStatus } from '@nestjs/common';
+
+export class NestPolicyError extends HttpException {
+  constructor() {
+    super('Unauthorized', HttpStatus.UNAUTHORIZED);
+  }
+}
